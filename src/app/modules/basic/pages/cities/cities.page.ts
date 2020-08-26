eb241701336a43be0c403909fd5d8648137531d7
solmaz.shokri <solmaz.shokri.motlagh.73@gmail.com>
2020-08-26 12:40:02 -0700

    create city page whitout state

--- src/app/modules/basic/pages/cities/cities.page.ts
+++ src/app/modules/basic/pages/cities/cities.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TableComponent } from '@app/shared/components/table/table.component';
+import { Observable } from 'rxjs';
+import { BaseCity } from '../../model/basic.model';
+import { BasicService } from '../../business/basic.service';
+import { DialogFormService } from '@app/services/dialog-form.service';
+import { DialogFormConfig } from '@app/shared/models/dialog-form-config';
 
 @Component({
   selector: 'cities',
@@ -7,9 +13,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CitiesPage implements OnInit {
 
-  constructor() { }
+  @ViewChild(TableComponent, { static: true }) table: TableComponent;
+
+  rowData$: Observable<BaseCity[]>;
+  columnDefs = [
+    {
+      field: 'title',
+      headerName: 'عنوان',
+    },
+    {
+      field: 'isActive',
+      headerName: 'وضعیت',
+      cellEditor: 'agSelectCellEditor',
+      cellEditorParams: {
+        values: ['فعال', 'غیرفعال'],
+      },
+      cellRenderer: this.activityCellRenderer,
+    },
+  ];
+
+  constructor(
+    private basicService: BasicService,
+    private dialogFormService: DialogFormService
+  ) {}
 
   ngOnInit(): void {
+    this.rowData$ = this.basicService.select<BaseCity>('City');
   }
 
+  activityCellRenderer(params) {
+    return booleanCellRenderer(params.data.isActive);
+  }
+
+  addCity() {
+    this.dialogFormService
+      .show('افزودن شهر', this.formConfig())
+      .onClose.subscribe((city: BaseCity) => {
+        if (city)
+          this.basicService
+            .insert<BaseCity>('City', city)
+            .subscribe((res) => this.table.addTransaction(city));
+      });
+  }
+
+  formConfig(): DialogFormConfig[] {
+    return [
+      {
+        type: 'text',
+        label: 'عنوان',
+        labelWidth: 60,
+        formControlName: 'title',
+        errors: [{ type: 'required', message: 'این فیلد الزامیست' }],
+      },
+    ] as DialogFormConfig[];
+  }
+
+  onCellValueChanged(event) {
+    let updatedData: BaseCity = event.data;
+    let field: string = event.colDef.field;
+    let value: string = event.value;
+    if (field == 'isActive') {
+      if (value === 'فعال') {
+        updatedData.isActive = true;
+        this.basicService
+          .activate('City', updatedData)
+          .subscribe(() => this.table.updateTransaction(updatedData));
+      } else if (value === 'غیرفعال') {
+        updatedData.isActive = false;
+        this.basicService
+          .deactivate('City', updatedData)
+          .subscribe(() => this.table.updateTransaction(updatedData));
+      }
+    } else
+    this.basicService
+      .update<BaseCity>('City', updatedData)
+      .subscribe(() => this.table.updateTransaction(updatedData));
+  }
+}
+
+function booleanCellRenderer(condtion: any) {
+  return `<div class="d-flex"><div style="width:15px;height:15px;border-radius:50%;margin-top:13px;background-color:${
+    condtion ? 'green' : 'red'
+  }"></div> <span>${condtion ? 'فعال' : 'غیرفعال'}</span></div>`;
 }
